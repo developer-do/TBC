68b257e11cbf5d4e61d231bf24f90f1f9d4edf8b
dev-do <dev-do@naver.com>
2020-12-13 01:04:19 +0900

    c 언어의 기초 공부

---
+++ MyFirstProj/MyFirstSolution/MyThirdProj/my_third_program.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("I love programming");
+
+	return 0;
+}
---
+++ variable/LectureSolution/Lecture1/Lecture1.cpp
@@ -0,0 +1,22 @@
+int main()
+{
+	int Num_Apples;
+	int My_love;
+	int house;
+	int cracker;
+	int a;
+	int A;
+
+	int x; // declaration (선언)
+	int y;
+	int z;
+
+	// int a;
+
+	x = 1; // assignment (할당)
+	y = 2;
+
+	z = x + y;
+
+	return 0;
+}
