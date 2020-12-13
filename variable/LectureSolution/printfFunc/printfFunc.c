69fafe4f67ce45586507b1ba4b7db8a6c91eed64
dev-do <dev-do@naver.com>
2020-12-13 22:45:46 +0900

    created simple program

--- variable/LectureSolution/printfFunc/printfFunc.c
+++ variable/LectureSolution/printfFunc/printfFunc.c
@@ -1,37 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void say_hello(void);
 
-int main() {
+int main() 
+{
+	float won = 0.0f;
+	float dollar = 0.0f;
 
+	printf("Input Won\n");
+	scanf("%f", &won);
 
-	say_hello();
+	dollar = won * 0.00092f;
 
-	// print formatted - 형식을 맞추다.
-	printf("\"The truth is ... \nI am Ironman.\"\n");
-	// escape sequence \n = new line
-
-	int x, y, sum;
-
-	x = 2321;
-	y = 3254;
-
-	sum = x + y;
-
-	printf("\nThe anwser is %i.\n", sum);
-
-	printf("\n%i + %i = %i\n", x, y, sum);
-	// printf("\a");
-
-	/*
-		
-	*/
+	printf("Dollar = %f\n", dollar);
 
 	return 0;
-}
-
-void say_hello(void) {
-	printf("Hello, World\n");
-	
-	return;
 }
