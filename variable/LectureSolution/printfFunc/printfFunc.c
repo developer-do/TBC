e8a5bbef746717308560ef8c609b94b7e64a1b40
dev-do <dev-do@naver.com>
2020-12-13 18:52:37 +0900

    printf usage

---
+++ variable/LectureSolution/printfFunc/printfFunc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+	// print formatted - 형식을 맞추다.
+	printf("\"The truth is ... \nI am Ironman.\"\n");
+	// escape sequence \n = new line
+
+	int x, y, z;
+
+	x = 2321;
+	y = 3254;
+
+	z = x + y;
+
+	printf("\nThe anwser is %i.\n", z);
+
+	printf("\n%i + %i = %i\n", x, y, z);
+	printf("\a");
+
+	return 0;
+}
