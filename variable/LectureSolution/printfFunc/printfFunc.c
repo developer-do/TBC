7777cb46652cf78a291fe9f7eb3138f88318d1f2
dev-do <dev-do@naver.com>
2020-12-15 00:43:30 +0900

    int format overflow

--- variable/LectureSolution/printfFunc/printfFunc.c
+++ variable/LectureSolution/printfFunc/printfFunc.c
@@ -1,18 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <climits>
+#include <stdlib.h>
 
 
 int main() 
 {
-	float won = 0.0f;
-	float dollar = 0.0f;
+	unsigned int u_max = UINT_MAX + 2;
+	unsigned int u_min = 0;
+	signed int i_max = INT_MAX;
+	signed int i_min = INT_MIN;
 
-	printf("Input Won\n");
-	scanf("%f", &won);
+	// printf("%u\n", sizeof(unsigned int));
+	printf("max of uint = %u\n", u_max);
+	printf("min of uint = %u\n", u_min);
+	printf("max of int = %d\n", i_max);
+	printf("min of int = %d\n", i_min);
 
-	dollar = won * 0.00092f;
+	// i to binary representation
+	char buffer[33];
+	_itoa(u_max, buffer, 2);
 
-	printf("Dollar = %f\n", dollar);
+	// print decimal and binary
+	printf("decimal: %u\n", u_max);
+	printf("binary: %s\n", buffer);
 
 	return 0;
 }
